39b399be84984f94d91beaae282fc12c573e00cf
bjick84 <bjick@web.de>
2023-01-23 10:24:04 +0100

    nested Arrays und nested loops

--- script_01.js
+++ script_01.js
@@ -7,6 +7,26 @@
 // output(arr[0]);
 // output("------------");
 
+// nested arrays [[[]]]
+// nested arrays [[],[]]
+arr =   [
+                 ["Ich","bin","Max"],
+                 ["Ich","bin","Moritz"]
+        ];
+
+// output(arr);
+// output(arr[0]);
+// output(arr[0][2]);
+// output(arr[0][1] + " " + arr[1][2]);
+
+// nested loop
+for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr[i].length; j++) {
+                output("Index i: "+i+"Index j: "+j)
+                output(arr[i][j]);
+                
+        }
+}
 
 /***** Objekte 1 Daten/Funktionen *******/
 
