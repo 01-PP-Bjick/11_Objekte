c92e9e0fbcccf6542bc794b08e4887b21862e8b3
bjick84 <bjick@web.de>
2023-01-23 11:19:15 +0100

    Objekte 1 Daten/Funktionen

--- script_01.js
+++ script_01.js
@@ -9,29 +9,51 @@
 
 // nested arrays [[[]]]
 // nested arrays [[],[]]
-arr =   [
-                 ["Ich","bin","Max"],
-                 ["Ich","bin","Moritz"]
-        ];
+// arr =   [
+//                  ["Ich","bin","Max"],
+//                  ["Ich","bin","Moritz"]
+//         ];
 
-// output(arr);
-// output(arr[0]);
-// output(arr[0][2]);
-// output(arr[0][1] + " " + arr[1][2]);
+// // output(arr);
+// // output(arr[0]);
+// // output(arr[0][2]);
+// // output(arr[0][1] + " " + arr[1][2]);
 
-// nested loop
-for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-                output("Index i: "+i+"Index j: "+j)
-                output(arr[i][j]);
+// // nested loop
+// for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr[i].length; j++) {
+//                 output("Index i: "+i+"Index j: "+j)
+//                 output(arr[i][j]);
                 
-        }
-}
+//         }
+// }
 
 /***** Objekte 1 Daten/Funktionen *******/
+let person =    {
+                        firstName:"Thomas",
+                        familyName:"Korn",
+                        salary: [120000,160000],
+                        permission: true,
+                        sayHello:
+                                function(){
+                                        return "Hello, ich bin "+
+                                        this.firstName+"!";
+                                }
+                };
 
+// output(person);
+// output(person.firstName);
+// output(person.familyName);
+// output(person.salary[1]);
+// output(person.permission);
+// output(person.sayHello());
 
+const txt =      person.sayHello() + " Ich bin " + person.firstName + " " +
+                 person.familyName +
+                 " und verdiene " +
+                 person.salary[1] + " p.a";
 
+output(txt)
 
 
 /** Ausgabe */
